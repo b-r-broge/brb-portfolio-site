73e80a492e955645a1674e515a369c042089115f
Brad Broge <brbroge@gmail.com>
2017-08-29 14:15:11 -0400

    styling updates, adding mission statement

--- src/components/App.js
+++ src/components/App.js
@@ -16,10 +16,13 @@ class App extends Component {
     return (
       <Baselayout>
         <div className="body">
-          <div className="bio">
+          <div id="mission" className="bio">
+            <h1> {"< Software Engineer />"} </h1>
+            <h2> Full Stack Web Development Solutions</h2>
+            <p>Motivated professional with experience in complete software life-cycle; requirements analysis, design, object-oriented development, and client support. Capable of troubleshooting complex JavaScript, Python, hardware, or operating system problems. Approachable, hardworking and capable of synthesizing complex ideas into consumable information for technical and non-technical personnel. Comfortable in a fast paced, Agile environment, and excels where deep team work and dedication to finding a solution is key.</p>
 
           </div>
-          <div className="projects">
+          <div id="examples" className="projects">
             <Glowbox title="Sound Cloud App" content="This was the last week of frontend with a focus on building a functional website to be built dynamically using ES6 Fetch API calls to the SoundCloud API.  I used Vue.js as a library to add the reusability of templates for several of the components." tech=" VueJS | Fetch | HTML | JavaScript | CSS " img={soundImg} live="https://b-r-broge.github.io/TIY-brb-week-four-project/" gh="https://github.com/b-r-broge/TIY-brb-week-four-project" />
             <Glowbox title="Mystery Word" content="Mystery Word is a simple game developed using NodeJS, Express and Mustaches to provide a full game with a score screen/leaderboard running on Postgres in the backend." tech=" NodeJS | Express | Mustaches | Postgres | Sequelize | CSS | Heroku " img={mysteryImg} live="https://hidden-mountain-92251.herokuapp.com/start" gh="https://github.com/b-r-broge/tiy-week5-mysteryword" />
           </div>
