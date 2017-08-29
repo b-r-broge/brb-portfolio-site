cc48dc5ed36bff511a0d51f619421e0ed4338a00
Brad Broge <brbroge@gmail.com>
2017-08-29 12:27:03 -0400

    adding soundcloud and updates to css

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import '../styles/App.css';
 import '../styles/font-awesome-4.7.0/css/font-awesome.css'
 
 const mysteryImg = require('../assets/mysteryWord.png');
+const soundImg = require('../assets/soundcloud.png')
 
 class App extends Component {
   render() {
@@ -18,8 +19,8 @@ class App extends Component {
           </div>
           <div className="projects">
             <Glowbox title="BLAMM! Store" content="This box is for the BLAMM! Store project description" img="" live="" gh="" />
-            <Glowbox title="Gobble Social Media" content="This is the second glowbox" img="" live="" gh="" />
-            <Glowbox title="Mystery Word" content="This is the description of the Mystery Word Project built in nodeJS" tech="NodeJS | Mustaches | Postgres | Sequelize" img={mysteryImg} live="https://hidden-mountain-92251.herokuapp.com/start" gh="https://github.com/b-r-broge/tiy-week5-mysteryword" />
+            <Glowbox title="Sound Cloud App" content="This was the last week of frontend with a focus on building a functional website to be built dynamically using ES6 Fetch API calls to the SoundCloud API.  I used Vue.js as a library to add the reusability of templates for several of the components." tech=" VueJS | Fetch | HTML | JavaScript | CSS " img={soundImg} live="https://b-r-broge.github.io/TIY-brb-week-four-project/" gh="https://github.com/b-r-broge/TIY-brb-week-four-project" />
+            <Glowbox title="Mystery Word" content="Mystery Word is a simple game developed using NodeJS, Express and Mustaches to provide a full game with a score screen/leaderboard running on Postgres in the backend." tech=" NodeJS | Express | Mustaches | Postgres | Sequelize | CSS | Heroku " img={mysteryImg} live="https://hidden-mountain-92251.herokuapp.com/start" gh="https://github.com/b-r-broge/tiy-week5-mysteryword" />
           </div>
         </div>
       </Baselayout>
