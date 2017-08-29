bbe0ed43d6a8837a6166235a97b2be828b8518a0
Brad Broge <brbroge@gmail.com>
2017-08-29 12:51:02 -0400

    adding resume and alerts

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,8 @@ const soundImg = require('../assets/soundcloud.png')
 
 class App extends Component {
   render() {
+    // <Glowbox title="BLAMM! Store" content="This box is for the BLAMM! Store project description" img="" live="" gh="" />
+
     return (
       <Baselayout>
         <div className="body">
@@ -18,7 +20,6 @@ class App extends Component {
 
           </div>
           <div className="projects">
-            <Glowbox title="BLAMM! Store" content="This box is for the BLAMM! Store project description" img="" live="" gh="" />
             <Glowbox title="Sound Cloud App" content="This was the last week of frontend with a focus on building a functional website to be built dynamically using ES6 Fetch API calls to the SoundCloud API.  I used Vue.js as a library to add the reusability of templates for several of the components." tech=" VueJS | Fetch | HTML | JavaScript | CSS " img={soundImg} live="https://b-r-broge.github.io/TIY-brb-week-four-project/" gh="https://github.com/b-r-broge/TIY-brb-week-four-project" />
             <Glowbox title="Mystery Word" content="Mystery Word is a simple game developed using NodeJS, Express and Mustaches to provide a full game with a score screen/leaderboard running on Postgres in the backend." tech=" NodeJS | Express | Mustaches | Postgres | Sequelize | CSS | Heroku " img={mysteryImg} live="https://hidden-mountain-92251.herokuapp.com/start" gh="https://github.com/b-r-broge/tiy-week5-mysteryword" />
           </div>
