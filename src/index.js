f7ec76c96d52df847ce0ef402d1b8471821244b6
Brad Broge <brbroge@gmail.com>
2017-08-29 21:55:10 -0400

    changes to allow deployment to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './styles/normalize.css';
 import './styles/index.css';
 
 import App from './components/App';
-import Project from './components/Project'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/:project" component={Project} />
-      <Route path="/" component={App} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById('root')
-  );
+ReactDOM.render(<App />, document.getElementById('root'));
